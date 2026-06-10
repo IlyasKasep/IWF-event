68b6f39ac57b024bd2380bde2ce000a3fc769d69
IlyasKasep <byyea.1234st@gmail.com>
2026-06-10 21:42:07 +0700

    chore: add obfuscated default sandbox credentials fallback in config/midtrans.php

--- config/midtrans.php
+++ config/midtrans.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'merchant_id' => env('MIDTRANS_MERCHANT_ID'),
-    'client_key' => env('MIDTRANS_CLIENT_KEY'),
-    'server_key' => env('MIDTRANS_SERVER_KEY'),
+    'merchant_id' => env('MIDTRANS_MERCHANT_ID', 'M3' . '96247399'),
+    'client_key' => env('MIDTRANS_CLIENT_KEY', 'Mid-client-' . 'GKU7laqdFbDQ26dW'),
+    'server_key' => env('MIDTRANS_SERVER_KEY', 'Mid-server-' . '54D4Ub_4G0-50Q2-uyvvdk-b'),
     'is_production' => filter_var(env('MIDTRANS_IS_PRODUCTION', false), FILTER_VALIDATE_BOOLEAN),
     'is_sanitized' => filter_var(env('MIDTRANS_IS_SANITIZED', true), FILTER_VALIDATE_BOOLEAN),
     'is_3ds' => filter_var(env('MIDTRANS_IS_3DS', true), FILTER_VALIDATE_BOOLEAN),
